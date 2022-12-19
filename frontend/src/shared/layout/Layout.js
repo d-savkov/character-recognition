57f3742015350d43a521684e68f78bb541eee0c8
Savkov Danila <savoooook@gmail.com>
2022-12-19 18:49:43 +0300

    frontend: Remove deprecated logo

--- frontend/src/shared/layout/Layout.js
+++ frontend/src/shared/layout/Layout.js
@@ -1,13 +1,9 @@
-import {Link, NavLink, Outlet} from 'react-router-dom';
-import logo from '../../icons/logo.svg';
+import {NavLink, Outlet} from 'react-router-dom';
 import './layout.css';
 
 const Layout = () => {
     return (<>
         <div className="header">
-            <Link to="/show" className="logo">
-                <img src={logo} height={30} alt="logo"/>
-            </Link>
             <div className="header-right">
                 <NavLink to="/show">Show</NavLink>
                 <NavLink to="/recognition">Recognition</NavLink>
